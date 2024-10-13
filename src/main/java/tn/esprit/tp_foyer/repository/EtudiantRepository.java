44de7eb99a1e74bad2d770bc3bbde797edfbe47a
hechmi10 <hechmi.jaidane@gmail.com>
2024-10-13 13:04:09 +0100

    créer des repositoires JPA des entités

--- src/main/java/tn/esprit/tp_foyer/repository/EtudiantRepository.java
+++ src/main/java/tn/esprit/tp_foyer/repository/EtudiantRepository.java
@@ -1,7 +1,9 @@
 package tn.esprit.tp_foyer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tn.esprit.tp_foyer.entity.Etudiant;
 
+@Repository
 public interface EtudiantRepository extends JpaRepository<Etudiant, Long> {
 }
--- src/main/java/tn/esprit/tp_foyer/repository/UniversiteRepository.java
+++ src/main/java/tn/esprit/tp_foyer/repository/UniversiteRepository.java
@@ -1,7 +1,9 @@
 package tn.esprit.tp_foyer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tn.esprit.tp_foyer.entity.Universite;
 
+@Repository
 public interface UniversiteRepository extends JpaRepository<Universite, Long> {
 }
