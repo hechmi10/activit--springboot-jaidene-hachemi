664a5206e2bb6ba706ccf28aa784747c7404433a
hechmi10 <hechmi.jaidane@gmail.com>
2024-11-07 14:54:34 +0100

    Controleurs et services

--- src/main/java/tn/esprit/tp_foyer/repository/BlocRepository.java
+++ src/main/java/tn/esprit/tp_foyer/repository/BlocRepository.java
@@ -1,7 +1,6 @@
 package tn.esprit.tp_foyer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import tn.esprit.tp_foyer.entity.Bloc;
 
